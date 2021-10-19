69d1a2d2e0eac7a234d0e139c04a30c4de1b83b2
sndmcglnchy <sineadmcg.18@gmail.com>
2021-10-19 15:55:51 +0100

    added functionality

--- js/script.js
+++ js/script.js
@@ -1,35 +1,4 @@
-const add = function(num1, num2) {
-	let first = parseInt(num1, 10);
-    let second = parseInt(num2, 10);
-    let answer = first + second;
-   answerText.textContent =  answer;
 
-};
-
-const subtract = function(num1, num2) {
-    let first = parseInt(num1, 10);
-    let second = parseInt(num2, 10);
-	let answer = first - second;
-
-    answerText.textContent =  answer;
-};
-
-  
-const multiply = function(num1, num2) {
-    let first = parseInt(num1, 10);
-    let second = parseInt(num2, 10);
-    let answer = first * second;
-
-    answerText.textContent =  answer;
-};
-
-const divide = function(num1, num2){
-    let first = parseInt(num1, 10);
-    let second = parseInt(num2, 10);
-    let answer = first / second;
-
-    answerText.textContent =  answer;
-}
   
 let equals = document.querySelector(".equals");
 let clear = document.querySelector(".clear");
@@ -44,21 +13,62 @@ let operatorPressed;
 let operatorIndex;
 let number1;
 let number2;
+let answer; 
+
+//EQUATIONS
+const add = function(num1, num2) {
+    let first = parseInt(num1, 10);
+    let second = parseInt(num2, 10);
+    answer = first + second;
+    answerText.textContent =  answer;
+
+};
+
+const subtract = function(num1, num2) {
+    let first = parseInt(num1, 10);
+    let second = parseInt(num2, 10);
+	answer = first - second;
+
+    answerText.textContent =  answer;
+};
+  
+const multiply = function(num1, num2) {
+    let first = parseInt(num1, 10);
+    let second = parseInt(num2, 10);
+    answer = first * second;
+
+    answerText.textContent =  answer;
+};
+
+const divide = function(num1, num2){
+    let first = parseInt(num1, 10);
+    let second = parseInt(num2, 10);
+    answer = first / second;
+
+    answerText.textContent =  answer;
+}
+//END OF BASIC EQUATIONS
 
 //WHEN ANY BUTTON IS CLICKED, ADD THAT BUTTON'S id TO AN ARRAY CALLED INPUT
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        input.push(button.id);
-   
+        input.push(button.id);   
 //print the array as a dynamic string onto the calculator's display
         calculateText.textContent = input.join(" ");
+        console.log(input);
 //get first part of equation 
-
+    // if (typeof answer === 'undefined') {
         number1 = input.slice(0, operatorIndex).join("");
-        console.log(number1);
+        
+        // console.log(number1);
+        //get second part of equation 
         number2 = input.slice(operatorIndex + 1).join("");
-        console.log(number2);
- 
+        // console.log(number2);
+    // }
+
+        // number1 = answer;
+        // number2 = input.slice(operatorIndex - 1).join("");
+
 
     });
 });
@@ -76,7 +86,6 @@ function checkOperator(operator) {
 }
 
 equals.addEventListener('click', () => {
-    console.log('Equals...')
     operate(operatorPressed, number1, number2);
 })
 
